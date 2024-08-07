0551ba060246742a9f1a4441cfa3d19b13a592c2
Kei Takahashi <ktakahashi@minitab.com>
2024-08-07 15:16:22 -0400

    Add compile time option to opt out FILE_ATTRIUBTE_REPARSE_POINT check

--- lib/zip_source_file_win32_named.c
+++ lib/zip_source_file_win32_named.c
@@ -33,6 +33,20 @@
 
 #include "zip_source_file_win32.h"
 
+/* ACL is not available when targeting the games API partition */
+#if defined(WINAPI_FAMILY_PARTITION) && defined(WINAPI_PARTITION_GAMES)
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_GAMES)
+#define ACL_UNSUPPORTED
+#endif
+#endif
+
+/* Optionally, ignore the REPARSE_POINT file attribute when determining if the source is a regular file or not */
+#if defined(WINAPI_IGNORE_REPARSE_POINT)
+#define REPARSE_POINT 0
+#elseif
+#define REPARSE_POINT FILE_ATTRIBUTE_REPARSE_POINT
+#endif
+
 static zip_int64_t _zip_win32_named_op_commit_write(zip_source_file_context_t *ctx);
 static zip_int64_t _zip_win32_named_op_create_temp_output(zip_source_file_context_t *ctx);
 static bool _zip_win32_named_op_open(zip_source_file_context_t *ctx);
@@ -210,7 +224,7 @@ _zip_win32_named_op_stat(zip_source_file_context_t *ctx, zip_source_file_stat_t 
     st->regular_file = false;
 
     if (file_attributes.dwFileAttributes != INVALID_FILE_ATTRIBUTES) {
-        if ((file_attributes.dwFileAttributes & (FILE_ATTRIBUTE_DIRECTORY | FILE_ATTRIBUTE_DEVICE | FILE_ATTRIBUTE_REPARSE_POINT)) == 0) {
+        if ((file_attributes.dwFileAttributes & (FILE_ATTRIBUTE_DIRECTORY | FILE_ATTRIBUTE_DEVICE | REPARSE_POINT)) == 0) {
             st->regular_file = true;
         }
     }
